1e2060132b06b900839b0207f9349c310d262731
Samuel Margerison <SamR.Margerison@gmail.com>
2024-06-21 15:59:37 +0100

    Initial Commit

---
+++ WEEKS/WEEKSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WEEKSApp.swift
+//  WEEKS
+//
+//  Created by Samuel Marg on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WEEKSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
